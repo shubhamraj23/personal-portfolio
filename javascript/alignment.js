5119a1084ac02860a58806303c9e378c6394c421
Shubham Raj Pandit <shubhamraj465@gmail.com>
2021-04-29 12:32:41 +0530

    Improved UI and interactions
    Added sticky element to tabs in small screen
    Added links to open tabs using carousel
    Added +/- icons and links to open tab on small screen using them

--- javascript/alignment.js
+++ javascript/alignment.js
@@ -1,11 +1,22 @@
 function alignNav(){
+    // Set carousel just below navbar
     var nav = document.getElementsByTagName("nav")[0];
     var height = nav.offsetHeight;
     var carousel = document.getElementsByClassName("carousel")[0];
     carousel.style.marginTop = height + "px";
 
+    // Set the height of sticky element in tabs
     var tab = document.getElementsByClassName("project-tab-large")[0];
     var offsetHeight = height - 2;
     tab.style.top = offsetHeight + "px";
+
+    // Align the plus and minus icons
+    var projects = ["testing", "automation", "cds", "dwc"];
+    for(var i=0; i<projects.length; i++){
+        var icons = document.getElementsByClassName('project-icons')[i];
+        var height = document.getElementById('project-' + projects[i] + '-copy-small').offsetHeight;
+        var offsetHeight = height/2 - icons.offsetHeight/2;
+        icons.style.bottom = offsetHeight + "px";
+    }
     return true;
 }
